070d2772558982602498a2c53aed5d9fa41b3e73
daveasiamah <daveasiamah@gmail.com>
2020-01-04 04:08:35 +0000

    Updates

---
+++ precache-manifest.10e56ef24876edaffda870f2a9958cee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6306e749165eacee6ba540c179b9e173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c778262d86ece7a6a6fc",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "34e32351d49fc6044229",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c778262d86ece7a6a6fc",
+    "url": "/static/js/2.984b24e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.984b24e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34e32351d49fc6044229",
+    "url": "/static/js/main.f0f5b27f.chunk.js"
+  },
+  {
+    "revision": "7353f6bb3502df7b77af",
+    "url": "/static/js/runtime-main.ef4affaf.js"
+  }
+]);
